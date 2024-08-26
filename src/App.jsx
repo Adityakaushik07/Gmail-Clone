548f8a94b443682465253c19ac1fbca6e0180d73
Aditya Kaushik <kaushikaditya985@gmail.com>
2024-08-26 12:23:48 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/shared/Navbar";
+
+function App() {
+  return (
+    <div className="bg-[#F6F8FC] w-screen h-screen overflow-hidden">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
